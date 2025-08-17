76bf85f299519cd26bfdce41f5bebafcefa638d2
EloToJaa <elotoja@protonmail.com>
2025-08-17 19:01:06 +0200

    Update config

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # nixos-anywhere-examples
 
 Checkout the [flake.nix](flake.nix) for examples tested on different hosters.
+
+## Run nixos-anywhere
+
+```bash
+nix run nixpkgs#nixos-anywhere -- \
+    --flake .#generic \
+    --generate-hardware-config nixos-generate-config ./hardware-configuration.nix \
+    root@192.168.0.10
+```
--- configuration.nix
+++ configuration.nix
@@ -3,13 +3,13 @@
   lib,
   pkgs,
   ...
-} @ args:
-{
+} @ args: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
   ];
+
   boot.loader.grub = {
     # no need to set devices, disko will add all devices that have a EF02 partition to the list already
     # devices = [ ];
@@ -21,13 +21,15 @@
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
+    pkgs.neovim
   ];
 
   users.users.root.openssh.authorizedKeys.keys =
-  [
-    # change this to your ssh key
-    "# CHANGE"
-  ] ++ (args.extraPublicKeys or []); # this is used for unit-testing this module and can be removed if not needed
+    [
+      # change this to your ssh key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMtWaehMf7X23uUZDY5J4fG4/exqj5jWQVaLLXloaO/g elotoja@protonmail.com"
+    ]
+    ++ (args.extraPublicKeys or []);
 
   system.stateVersion = "24.05";
 }
